a7f45dba3b98b9ce323c9b85a25037e5cd19578c
Javier  Alonso <sicotic87@gmail.com>
2025-07-28 20:53:24 -0400

    appointment object requirements

--- src/Appointment.java
+++ src/Appointment.java
@@ -3,12 +3,26 @@ import java.util.Date;
 public class Appointment {
 
     //members
-    public String appointmentId;
+    public static String appointmentId;
     public Date appointmentDate;
     public String description;
 
     public Appointment(String appointmentId, Date appointmentDate, String description) {
 
+        //Appointment date shall not be null or greater than 10 characters
+        if(appointmentId == null || appointmentId.length() > 10) {
+            throw new IllegalArgumentException("Invalid appointment ID.");
+        }
+
+        //Appointment date shall not be null or in the past
+        if(appointmentDate == null || appointmentDate.before(new Date())) {
+            throw new IllegalArgumentException("Invalid appointment date.");
+        }
+
+        //Description field shall not be null or greater than 50 characters
+        if(description == null || description.length() > 50) {
+            throw new IllegalArgumentException("Invalid description.");
+        }
     }
 
 }
