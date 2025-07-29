3d4185a3999e8339e6b324cd830b3d945859cd63
Javier  Alonso <sicotic87@gmail.com>
2025-07-28 21:30:53 -0400

    begin writing unit tests

--- src/AppointmentTest.java
+++ src/AppointmentTest.java
@@ -1,2 +1,15 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import java.util.Date;
+
 public class AppointmentTest {
+
+    //verify ability to create new appointment
+    @Test
+    void testAppointmentCreation() {
+        Appointment appointment = new Appointment("1", new Date() , "Description");
+        assertEquals("1", appointment.getAppointmentId());
+    }
+
+
 }
